86175945095c09dc309dda500d4352b6845b3448
Gabriel-Dolce <98546482+Gabriel-Dolce@users.noreply.github.com>
2024-03-26 15:14:01 -0400

    Add files via upload
    fixed LED colors

--- DeviceTest.ino
+++ DeviceTest.ino
@@ -15,15 +15,15 @@ const char* password = "";
 
 // Define NTP settings
 const char* ntpServer = "pool.ntp.org";
-const int timeZone = -5; // Eastern Standard Time (EST)
+const int timeZone = -4; // Eastern Standard Time (EST)
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, ntpServer, timeZone * 3600, 60000);
 
 // Define pins for StatusLight
-const int PIN_RED = 25;    // Red
-const int PIN_GREEN = 33; // Green
-const int PIN_BLUE = 32;  // Blue
+const int PIN_GREEN = 25;    // Red
+const int PIN_RED = 33;  // Green
+const int PIN_BLUE = 32;   // Blue
 StatusLight myStatusLight(PIN_RED, PIN_GREEN, PIN_BLUE);
 
 Adafruit_SHT4x sht4 = Adafruit_SHT4x();
@@ -33,8 +33,13 @@ void setup() {
 
   // Initialize LCD
   lcd.begin(16, 2); // Adjust according to your LCD's dimensions
+
+  // Setup StatusLight
+  myStatusLight.setupStatusLight();
+
   lcd.print("Initializing...");
 
+  myStatusLight.setSolid(PIN_RED); // Red before connected to wifi
   // Connect to WiFi
   connectToWiFi();
 
@@ -42,20 +47,25 @@ void setup() {
   timeClient.begin();
   timeClient.update();
 
-  // Setup StatusLight
-  myStatusLight.setupStatusLight();
+  
 
   // Setup SHT4x sensor
   if (!sht4.begin()) {
     Serial.println("Couldn't find SHT4x");
     while (1);
+    delay(500);
   }
   Serial.println("Found SHT4x sensor");
 }
 
+// Flag to track whether NTP time has been successfully updated
+bool ntpUpdated = false;
+
 void loop() {
   // Update time from NTP server
-  timeClient.update();
+  if (!ntpUpdated) {
+    ntpUpdated = timeClient.update();
+  }
 
   // Get current time
   unsigned long epochTime = timeClient.getEpochTime();
@@ -63,7 +73,7 @@ void loop() {
 
   // Format time
   char timeStr[9];
-  sprintf(timeStr, "%02d:%02d:%02d", currentTime->tm_hour, currentTime->tm_min, currentTime->tm_sec);
+  sprintf(timeStr, "%02d:%02d", currentTime->tm_hour, currentTime->tm_min);
 
   // Display time on LCD
   lcd.clear();
@@ -82,15 +92,21 @@ void loop() {
   lcd.print(" C");
 
   // Update StatusLight based on WiFi and NTP connection
-  if (WiFi.status() != WL_CONNECTED) {
-    myStatusLight.setSolid(PIN_RED); // Red
-  } else if (!timeClient.update()) {
-    myStatusLight.setSolid(PIN_BLUE); // Blue
-  } else {
-    myStatusLight.setSolid(PIN_GREEN); // Green
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    myStatusLight.setSolid(PIN_RED); // Red = no wifi
+  } 
+  else if(!ntpUpdated)
+  {
+    myStatusLight.setSolid(PIN_GREEN); // Green = wifi but no server.
+    //server updates every minute. If just connected it has to wait a minute to turn blue
+  }
+  else
+  {
+    myStatusLight.setSolid(PIN_BLUE); // Blue = updates from server
   }
 
-  delay(1000); // Update every second
+  delay(10000); // Update every 10 seconds
 }
 
 void connectToWiFi() {
